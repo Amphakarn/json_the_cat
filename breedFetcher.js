9377f3a15b15f405ebca9d082d66a58b5ea4e7e0
Amphakarn <pisuthigomol@yahoo.com>
2020-12-03 23:43:11 +0000

    Refactor breedFetcher and add breedFetcherTests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,23 @@
 const request = require('request');
-const breed = process.argv[2];
 
-const url = "https://api.thecatapi.caaaaaaaa/v1/breeds/search?q=";
+const fetchBreedDescription = function(breedName, callback) {
+  const url = "https://api.thecatapi.com/v1/breeds/search?q=";
+  const breedURL = `${url}${breedName}`;
 
-const breedURL = `${url}${breed}`;
-
-request(breedURL, (error, response, body) => {  
-  if (error) {
-    console.log('URL invalid!', error);
-  } else {
-    if (data[0] === undefined) {
-      console.log(`Value does not exist!`);
-    }  else {
-      const data = JSON.parse(body);  // convert string to object
-      console.log(data[0].description); // printout value of the key description
+  request(breedURL, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     }
-  }
-});
+
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      callback(`Value does not exist!`);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
+
